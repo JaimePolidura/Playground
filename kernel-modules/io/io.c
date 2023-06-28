e1d093428d59a6faf5233545000774a8a02e20bf
Jaime <jaime.polidura@gmail.com>
2023-06-28 22:42:09 +0200

    finished with io

--- kernel-modules/io/io.c
+++ kernel-modules/io/io.c
@@ -3,27 +3,51 @@
 static unsigned long my_io_base = 0x378;
 static int my_io_major = 0;
 static unsigned long my_io_buffer = 0;
+static int my_io_irq = 5;
+
+DECLARE_WAIT_QUEUE_HEAD(my_io_queue);
 
 struct file_operations my_io_fops = {
 	.owner = THIS_MODULE,
     .read = io_read,
     .write = io_write,
+    .open = io_open,
 };
 
+irqreturn_t my_io_interrupt(int irq, void * dev_id) {    
+    outb(inb(my_io_base) & 0x7F, my_io_base); //Clearn device's pending interrupt bit
+    
+    printk(KERN_ALERT "Interrupt served!");
+    wake_up_interruptible(&my_io_queue);
+
+    return IRQ_HANDLED;
+}
+
+int io_open(struct inode * inode, struct file * file) {
+    file->private_data = kzalloc(sizeof(struct my_io_device), GFP_KERNEL);
+    
+    return 0;
+}
+
 ssize_t io_write(struct file * file, const char __user * buffer, size_t count, loff_t * f_pos) {
+    struct my_io_device * my_io_device = file->private_data;
     struct inode * inode = file_dentry(file)->d_inode;
     int minor = iminor(inode);
-    void * address_io_to_write = (void *) my_io_base + (minor & 0x0f);
+    void * address_io_to_write = (void *) my_io_base + (minor & 0x0F);
 
     unsigned char * ptr_data_to_write = kmalloc(count, GFP_KERNEL);
     copy_from_user(ptr_data_to_write, buffer, count);
     
     unsigned char * actual_ptr_data_to_write = ptr_data_to_write;
-    
+
+    my_io_device->can_read == 1;
+
     while (count--) {
         iowrite8(*actual_ptr_data_to_write++, address_io_to_write);
         wmb();
     }
+
+    //Raise interrupt (no se como xd)
 
     kfree(ptr_data_to_write);
 
@@ -31,6 +55,14 @@ ssize_t io_write(struct file * file, const char __user * buffer, size_t count, l
 }
 
 ssize_t io_read(struct file * file, char __user * buffer, size_t count, loff_t * f_pos) {
+    struct my_io_device * my_io_device = file->private_data;
+    
+    while(my_io_device->can_read == 0){
+        if(wait_event_interruptible(my_io_queue, my_io_device->can_read == 0)){
+            return -ERESTARTSYS;
+        }
+    }
+
     struct inode * inode = file_dentry(file)->d_inode;
     int minor = iminor(inode);
     void * address_io_to_read = (void *) my_io_base + (minor & 0x0f);
@@ -46,6 +78,7 @@ ssize_t io_read(struct file * file, char __user * buffer, size_t count, loff_t *
 
     copy_to_user(buffer, ptr_data_buffer, count);
     kfree(ptr_data_buffer);
+    my_io_device->can_read = 0;
 
     return 0;
 }
@@ -59,14 +92,20 @@ static int __init io_init(void) {
     my_io_major = register_chrdev(0, "my_io", &my_io_fops);
     my_io_buffer = __get_free_pages(GFP_KERNEL, 1);
 
+    request_irq(my_io_irq, my_io_interrupt, IRQF_SHARED, "my_io", my_io_interrupt);
+    outb(0x10, my_io_base + 2);
+
     return 0;
 }
 
 static void __exit io_exit(void) {
+    free_irq(my_io_irq, NULL);
+    outb(0x00, my_io_base + 2);
+
     release_mem_region(my_io_base, MY_IO_N_PORTS);
     iounmap(my_io_base);
     
-	unregister_chrdev(my_io_major, "short");
+	unregister_chrdev(my_io_major, "my_io");
 }
 
 MODULE_LICENSE("GPL");
--- kernel-modules/io/io.h
+++ kernel-modules/io/io.h
@@ -8,12 +8,20 @@
 #include <linux/ioport.h>
 #include <asm/io.h>
 #include <linux/ioport.h>
+#include <linux/interrupt.h>
 
 #define MY_IO_N_PORTS 8
 #define MY_IO_MINOR 0
 
+struct my_io_device {
+    int can_read;
+};
+
+irqreturn_t my_io_interrupt(int irq, void * dev_id);
+
 ssize_t io_read(struct file * file, char __user * buffer, size_t count, loff_t * f_pos);
 ssize_t io_write(struct file * file, const char __user * buffer, size_t count, loff_t * f_pos);
+int io_open(struct inode * inode, struct file * file);
 
 static int __init io_init(void);
 static void __exit io_exit(void);
