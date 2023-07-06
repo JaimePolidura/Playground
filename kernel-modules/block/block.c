10010f4276222376b45d7c554633335c5c206790
Jaime Polidura <73758994+JaimeTruman@users.noreply.github.com>
2023-07-06 21:32:25 +0200

    finished block

--- kernel-modules/block/block.c
+++ kernel-modules/block/block.c
@@ -2,7 +2,7 @@
 
 static struct my_block * my_block;
 
-static const struct block_device_operations fops = {
+static const struct block_device_operations block_ops = {
 	.owner = THIS_MODULE,
 	.open = block_open,
 	.release = block_release,
@@ -10,10 +10,114 @@ static const struct block_device_operations fops = {
 	.submit_bio = block_submit_bio,
 };
 
-static int block_open(struct block_device * bdev, fmode_t mode) {
-    struct my_block * my_block = bdev->bd_disk->private_data;
+static struct blk_mq_ops my_block_queue_ops = {
+    .queue_rq = block_queue_request,
+};
+
+blk_status_t block_queue_request(struct blk_mq_hw_ctx * blk_mq_hw_ctx, const struct blk_mq_queue_data * blk_mq_queue_data) {
+	struct request * request = blk_mq_queue_data->rq;
+	struct my_block * my_block = request->q->queuedata;
+    sector_t position_sector = blk_rq_pos(request);
+    struct req_iterator req_iterator;
+    struct bio_vec bio_vec;
+
+    blk_mq_start_request(request);
+
+    if(blk_rq_is_passthrough(request)) {
+        blk_mq_end_request(request, BLK_STS_IOERR);
+        return BLK_STS_IOERR;
+    }
+
+    rq_for_each_segment(bio_vec, request, req_iterator) {
+	    size_t sector_num = blk_rq_cur_sectors(request);
+        void * buffer = page_address(bio_vec.bv_page) + bio_vec.bv_offset;;
+        
+        block_transfer(my_block, position_sector, sector_num, buffer, rq_data_dir(request) == WRITE);
+
+        position_sector += sector_num;
+    }
+
+    blk_mq_end_request(request, BLK_STS_OK);
+
+    return BLK_STS_OK;
+}
+
+void block_submit_bio(struct bio * bio) {
+    struct my_block * my_block = bio->bi_bdev->bd_disk->private_data; 
+    loff_t position = bio->bi_iter.bi_sector << SECTOR_SHIFT;
+	struct bvec_iter bvec_iterator;
+    struct bio_vec bvec;
+
+    bio_for_each_segment(bvec, bio, bvec_iterator) {
+        unsigned int length = bvec.bv_len;
+        void * buffer = page_address(bvec.bv_page) + bvec.bv_offset;
+
+        block_transfer(my_block, length, position, buffer, bio_data_dir(bio));
+    }
     
+    bio_endio(bio);
+}
+
+void block_transfer(struct my_block * my_block, unsigned long sector, unsigned long sector_number, char * buffer, int write) {
+	unsigned long nbytes = sector_number * KERNEL_SECTOR_SIZE;
+    unsigned long offset = sector * KERNEL_SECTOR_SIZE;
+
+    if (write)
+		memcpy(my_block->data + offset, buffer, nbytes);
+	else
+		memcpy(my_block, my_block->data + offset, nbytes);
+}
+
+int block_ioctl (struct block_device * bdev, fmode_t mode, unsigned int cmd, unsigned long arg) {
+    struct my_block * my_block = bdev->bd_disk->private_data;
+
+    switch(cmd) {
+        case HDIO_GETGEO:
+            long size = my_block->size * (MY_BLOCK_SECTOR_SIZE / KERNEL_SECTOR_SIZE);
+
+            struct hd_geometry geo = {
+                .cylinders = (size & ~0x3F) >> 6,
+                .heads = 4,
+                .sectors = 16,
+                .start = 4
+            };
+
+            copy_to_user((void __user *) arg, &geo, sizeof(geo));
+
+            return 0;
+    }
+
+    return -ENOTTY;
+}
+
+static int block_open(struct block_device * bdev, fmode_t mode) {    
+    del_timer_sync(&my_block->timer);
+
+	spin_lock(&my_block->lock);
+    my_block->n_users++;
+    spin_unlock(&my_block->lock);
+
     return 0;
+}
+
+static void block_release(struct gendisk * disk, fmode_t mode) {
+    struct my_block * my_block = disk->private_data;
+
+    spin_lock(&my_block->lock);
+    my_block->n_users--;
+    if(my_block->n_users == 0){
+        my_block->timer.expires = jiffies + MY_BLOCK_INVALIDATE_DELAY;
+        add_timer(&my_block->timer);
+    }
+    spin_unlock(&my_block->lock);
+}
+
+void block_timer_timeout(struct timer_list * timer) {
+    struct my_block *my_block = from_timer(my_block, timer, timer);
+
+    spin_lock(&my_block->lock);
+    my_block->media_changed = 1;
+    spin_unlock(&my_block->lock);
 }
 
 static int __init block_init(void) {
@@ -21,7 +125,7 @@ static int __init block_init(void) {
 
     my_block = kzalloc(sizeof(struct my_block), GFP_KERNEL);
     spin_lock_init(&my_block->lock);
-
+    blk_mq_alloc_sq_tag_set(&my_block->tag_set, &my_block_queue_ops, 128, 0);
     my_block->size = MY_BLOCK_N_SECTORS * MY_BLOCK_SECTOR_SIZE;
     my_block->data = vmalloc(my_block->size);
     my_block->queue = blk_mq_init_queue(&my_block->tag_set);
@@ -33,6 +137,7 @@ static int __init block_init(void) {
     my_block->gendisk->private_data = my_block;
 	sprintf(my_block->gendisk->disk_name, "myblock");
     set_capacity(my_block->gendisk, MY_BLOCK_N_SECTORS * (MY_BLOCK_SECTOR_SIZE / KERNEL_SECTOR_SIZE));
+    timer_setup(&my_block->timer, block_timer_timeout, 0);
 
 	blk_queue_physical_block_size(my_block->queue, MY_BLOCK_SECTOR_SIZE);
 	blk_queue_logical_block_size(my_block->queue, MY_BLOCK_SECTOR_SIZE);
@@ -48,7 +153,7 @@ static void __exit block_exit(void) {
     del_gendisk(my_block->gendisk);
     put_disk(my_block->gendisk);
     vfree(my_block->data);
-    blk_cleanup_queue(my_block->queue);
+    //Clean queue
     unregister_blkdev(MY_BLOCK_MAJOR, "myblock");
     kfree(my_block);
 }
--- kernel-modules/block/block.h
+++ kernel-modules/block/block.h
@@ -18,12 +18,14 @@
 #include <linux/blk-mq.h>
 #include <linux/hdreg.h>
 #include <linux/blkdev.h>
+#include <linux/timer.h>
 
 #define KERNEL_SECTOR_SIZE	512
 #define MY_BLOCK_N_SECTORS 1024
 #define MY_BLOCK_SECTOR_SIZE 512
 #define MY_BLOCK_MINORS 16
 #define MY_BLOCK_MAJOR 0
+#define MY_BLOCK_INVALIDATE_DELAY 30 * HZ
 
 struct my_block {
     int size;
@@ -32,10 +34,20 @@ struct my_block {
     struct request_queue * queue;
     struct gendisk * gendisk;
     struct blk_mq_tag_set tag_set;
+    struct timer_list timer;
+    short media_changed;
+    short n_users;
 };
 
 static int block_open(struct block_device * bdev, fmode_t mode);
+static void block_release(struct gendisk *disk, fmode_t mode);
+int block_ioctl (struct block_device *bdev, fmode_t mode, unsigned int cmd, unsigned long arg);
+blk_status_t block_queue_request(struct blk_mq_hw_ctx * blk_mq_hw_ctx, const struct blk_mq_queue_data * blk_mq_queue_data);
+void block_submit_bio(struct bio * bio);
 
+void block_transfer(struct my_block * dev, unsigned long sector, unsigned long sector_number, char * buffer, int write);
+
+void block_timer_timeout(struct timer_list * ldev);
 extern void blk_cleanup_queue(struct request_queue * queue);
 
 static int __init block_init(void);
