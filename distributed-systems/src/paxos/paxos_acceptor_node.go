9c1e7ba347cc9a899c1e662ffed9509159902cde
Jaime Polidura <jaime.polidura@gmail.com>
2023-07-31 23:23:33 +0200

    Commited

--- distributed-systems/src/paxos/paxos_acceptor_node.go
+++ distributed-systems/src/paxos/paxos_acceptor_node.go
@@ -9,17 +9,17 @@ import (
 func (this *PaxosNode) handlePrepareMessage(message *nodes.Message) {
 	proposalId := message.GetContentToUint64()
 	proposerNodeId := message.NodeIdSender
-	value := message.SeqNum
+	valuePrepared := message.SeqNum
 
 	if proposalId < this.proposalIdPromised {
-		fmt.Printf("[%d] Ignoring received PREPARE(%d) from proposer node %d of value %d Promised to ignore ids lower than %d\n",
-			this.GetNodeId(), proposalId, message.NodeIdSender, value, this.proposalIdPromised)
+		fmt.Printf("[%d] Ignoring received PREPARE(%d) from proposer node %d of valuePrepared %d Promised to ignore ids lower than %d\n",
+			this.GetNodeId(), proposalId, message.NodeIdSender, valuePrepared, this.proposalIdPromised)
 		return
 	}
 
 	if !this.someProposalAccepted {
-		fmt.Printf("[%d] Received PREPARE(%d) from proposer node %d of value %d Sending back PROMISE(%d)\n",
-			this.GetNodeId(), proposalId, message.NodeIdSender, value, proposalId)
+		fmt.Printf("[%d] Received PREPARE(%d) from proposer node %d of valuePrepared %d Sending back PROMISE(%d)\n",
+			this.GetNodeId(), proposalId, message.NodeIdSender, valuePrepared, proposalId)
 
 		this.proposalIdPromised = proposalId
 
@@ -27,9 +27,9 @@ func (this *PaxosNode) handlePrepareMessage(message *nodes.Message) {
 			nodes.WithNodeId(this.GetNodeId()),
 			nodes.WithContentUInt64(proposalId),
 			nodes.WithType(types.MESSAGE_PAXOS_PROMISE),
-			nodes.WithSeqNum(value)))
+			nodes.WithSeqNum(valuePrepared)))
 	} else {
-		fmt.Printf("[%d] Received PREPARE(%d) from proposer node %d of value %d But already accepted proposal id with %d and value %d. Sending back PROMISE_ACCPET(%d, %d)\n",
+		fmt.Printf("[%d] Received PREPARE(%d) from proposer node %d of valuePrepared %d But already accepted proposal id with %d and valuePrepared %d. Sending back PROMISE_ACCPET(%d, %d)\n",
 			this.GetNodeId(), proposalId, message.NodeIdSender, this.proposalIdAccepted, this.lastValueAccepted, proposalId, this.proposalIdAccepted, proposalId, this.proposalIdAccepted)
 
 		this.GetConnectionManager().Send(proposerNodeId, nodes.CreateMessage(
